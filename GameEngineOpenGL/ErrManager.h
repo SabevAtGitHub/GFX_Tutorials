7d2058a929e90dad87d2873a3f90eb5074a08eb5
bezraboten34 <tt3d@start.bg>
2018-04-02 15:54:38 +0200

    errManager.h

--- GameEngineOpenGL/ErrManager.h
+++ GameEngineOpenGL/ErrManager.h
@@ -1,11 +1,24 @@
 #pragma once
 #include <string>
 
-
 namespace ge {
+
+#ifdef _DEBUG
+#define ASSERT(x) if (!(x)) __debugbreak();
+#define GLCall(x) GLClearError();\
+					x;\
+					ASSERT(GLLogCall(#x, __FILE__, __LINE__))
+#else 
+#define GLCall(x) x
+#endif // _DEBUG
+
 	/// <summary>
-/// Trowing error and ending the program
-/// <param name="errorString">Error message</param>
-/// </summary>
-	extern void fatalError(std::string errString);
+	/// Trowing error and ending the program
+	/// <param name="errorString">Error message</param>
+	/// </summary>
+	extern void fatalError(std::string errorString);
+
+	extern void GLClearError();
+
+	extern bool GLLogCall(const char* function, const char* file, int line);
 }
