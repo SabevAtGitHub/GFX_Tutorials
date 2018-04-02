950365801c9ca48211630d517cbadeb4b10fad40
bezraboten34 <tt3d@start.bg>
2018-04-02 15:57:01 +0200

    Errors updated with GLCall()

--- GameEngineOpenGL/Errors.cpp
+++ GameEngineOpenGL/Errors.cpp
@@ -1,9 +1,24 @@
 #include "ErrManager.h"
 #include <iostream>
 #include <SDL\SDL.h>
+#include <GL\glew.h>
 #include <cstdlib>
 
 namespace ge {
+
+	void GLClearError() {
+		while (glGetError() != GL_NO_ERROR);
+	}
+
+	bool GLLogCall(const char* function, const char* file, int line) {
+		if (GLenum error = glGetError()) {
+			std::cout << "[OpenGL Error] (0x" << std::hex << error << std::dec << "): " <<
+				function << ", File: " << file << ", Line: " << line << std::endl;
+			return false;
+		}
+		return true;
+	}
+
 	void fatalError(std::string errorString) {
 		std::cout << errorString << std::endl;
 		std::cout << "Press any key to exit...";
