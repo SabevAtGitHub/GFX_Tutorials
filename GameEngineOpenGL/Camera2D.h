a19b27f67a04c02a43e5bb6dfea2b595e3a8f79b
bezraboten34 <tt3d@start.bg>
2018-05-03 23:56:45 +0200

    ge::Camera2D Offset functions

--- GameEngineOpenGL/Camera2D.h
+++ GameEngineOpenGL/Camera2D.h
@@ -10,27 +10,20 @@ namespace ge {
 		~Camera2D();
 		// must be called before window.create();
 		void init(int screenWidth, int screenHeight);
-
 		void update();
-
 		glm::vec2 covertScreenToWorld(glm::vec2 screenCoords);
-
 		bool isInView(glm::vec2& Pos, const glm::vec2& Dim);
-
+		void offsetPosition(const glm::vec2& offset) { m_pos += offset; m_needsMatrixUpdate = true; }
+		void offsetScale(float offset) { m_scale += offset; if (m_scale < 0.001f) m_scale = 0.001f; m_needsMatrixUpdate = true; }
 		// setters
 		void setPosition(glm::vec2& newPos) { m_pos = newPos; m_needsMatrixUpdate = true; }
-
 		void setScale(float newScale) { m_scale = newScale; m_needsMatrixUpdate = true; }
-
 		// getters
 		glm::vec2 getPosition() { return m_pos; }
 		float getScale() { return m_scale; }
-		
 		glm::mat4 getCameraMatrix() { return m_cameraMatrix; }
 
-
 	private:
-
 		glm::vec2 m_pos;
 		glm::mat4 m_cameraMatrix; // 4x4 matrix
 		glm::mat4 m_orthoMatrix;
