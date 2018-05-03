b8a3f663ea47a23487ccf4c62aeaedb89b4b30d1
bezraboten34 <tt3d@start.bg>
2018-05-03 12:36:04 +0200

    vid 56, min 39:00

--- NinjaPlatformer/EditorScreen.cpp
+++ NinjaPlatformer/EditorScreen.cpp
@@ -1,9 +1,12 @@
 #include "EditorScreen.h"
 #include <SDL\SDL_events.h>
+#include <GameEngineOpenGL\ResourceManager.h>
 
 
 EditorScreen::EditorScreen(ge::Window* window)
-	: m_window(window)
+	:
+	m_window(window),
+	m_spriteFont("Fonts/Xcelsion.ttf", 32)
 {
 	m_screenIndex = SCREEN_INDEX_EDITOR;
 }
@@ -39,11 +42,18 @@ void EditorScreen::onEntry()
 	m_camera.setScale(CAMERA_SCALE);
 
 	initGUI();
+	m_spriteBatch.init();
+	
+	initShaders();
+
+	ge::ResourceManager::getTexture("Assets/blank.png");
+
 }
 
 void EditorScreen::onExit()
 {
 	m_gui.destroy();
+	m_textureProgram.dispose();
 }
 
 void EditorScreen::update()
@@ -57,42 +67,97 @@ void EditorScreen::draw()
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glClearColor(0.2f, 0.08f, 0.55f, 1.f); // red
 
+	{// drawing the color picker quad
+		m_spriteBatch.begin();
+
+		glm::vec4 destRect;
+		destRect.x = m_bSlider->getPosition().d_x.px + 20.0f; // some offset
+		destRect.y = m_bSlider->getPosition().d_y.px;
+		destRect.z = 50.0f;
+		destRect.w = 50.0f;
+
+		auto color = ge::ColorRGBA8((GLubyte)m_rColorVal, (GLubyte)m_gColorVal, (GLubyte)m_bColorVal, 255);
+
+		m_spriteBatch.draw(destRect, glm::vec4(0.0f, 0.0f, 1.0f, 1.0f), 1 , 0.0f, color);
+
+		m_spriteBatch.end();
+	}
+
 	m_gui.draw();
+
+}
+
+
+void EditorScreen::initShaders()
+{
+
+	// Compile the texture
+	m_textureProgram.compileShadersFromFile(std::string("Shaders/textureShading.vert"),
+		std::string("Shaders/textureShading.frag"));
+	m_textureProgram.addAttribute("vertexPos");
+	m_textureProgram.addAttribute("vertexColor");
+	m_textureProgram.addAttribute("vertexUV");
+	m_textureProgram.linkShaders();
 
 }
 
 void EditorScreen::initGUI()
 {
+	using namespace CEGUI;
+
 	//ge::GUI::init("GUI");
-	std::string schemeName = "TaharezLook";
+	std::string scheme = "TaharezLook"; //AlfiskoSkin
 
 	m_gui.init("GUI");
-	m_gui.loadScheme(schemeName);
+	m_gui.loadScheme(scheme);
 	m_gui.setFont("DejaVuSans-10");
 
-	std::string elementType = "Slider";
+#pragma region Sliders
+	{ // adding sliders
+		std::string typ = "Slider";
+		const float X_DIM = 0.017f, Y_DIM = 0.13f;
+		const float X_POS = 0.08f, Y_POS = 0.1f;
+		const float PADDING = 0.022f;
 
+		auto destRect = glm::vec4(X_POS, Y_POS, X_DIM, Y_DIM);
 
-#pragma region ColorPicker slider
+		m_rSlider = static_cast<Slider*>(
+			m_gui.createWidget(std::string(scheme + "/" + typ), destRect, glm::vec4(0.0f), "rSlider"));
+		m_rSlider->setMaxValue(255.f);
+		m_rSlider->setCurrentValue(m_rColorVal);
+		m_rSlider->subscribeEvent(Slider::EventValueChanged,
+			Event::Subscriber(&EditorScreen::onRedPickerChanged, this));
+		m_rSlider->setClickStep(1.0f);
 
-	auto elementPos = glm::vec4(0.2f, 0.20f, 0.02f, 0.2f);
-	std::string elementDescrib = { schemeName + "/" + elementType };
+		destRect = glm::vec4(X_POS + (X_DIM + PADDING) * 1, Y_POS, X_DIM, Y_DIM);
 
-	auto slider1 = static_cast<CEGUI::Slider*>(
-		m_gui.createWidget(elementDescrib, elementPos, glm::vec4(0.0f), "Slider1"));
-	slider1->setMaxValue(255.f);
-	slider1->setCurrentValue(m_colorPickerRed);
+		m_gSlider = static_cast<Slider*>(
+			m_gui.createWidget(std::string(scheme + "/" + typ), destRect, glm::vec4(0.0f), "gSlider"));
+		m_gSlider->setMaxValue(255.f);
+		m_gSlider->setCurrentValue(m_gColorVal);
+		m_gSlider->subscribeEvent(Slider::EventValueChanged,
+			Event::Subscriber(&EditorScreen::onGreenPickerChanged, this));
+		m_gSlider->setClickStep(1.0f);
 
+		destRect = glm::vec4(X_POS + (X_DIM + PADDING) * 2, Y_POS, X_DIM, Y_DIM);
 
+		m_bSlider = static_cast<Slider*>(
+			m_gui.createWidget(std::string(scheme + "/" + typ), destRect, glm::vec4(0.0f), "bSlider"));
+		m_bSlider->setMaxValue(255.f);
+		m_bSlider->setCurrentValue(m_bColorVal);
+		m_bSlider->subscribeEvent(Slider::EventValueChanged,
+			Event::Subscriber(&EditorScreen::onBluePickerChanged, this));
+		m_bSlider->setClickStep(1.0f);
+	}
 #pragma endregion
 
+
 	// set custom mouse cursor
-	m_gui.setMouseCursor(std::string(schemeName + "/MouseArrow"));
+	m_gui.setMouseCursor(std::string(scheme + "/MouseArrow"));
 	m_gui.showMouseCursor();
 	SDL_ShowCursor(0);
 
 }
-
 
 void EditorScreen::checkInput()
 {
@@ -109,10 +174,24 @@ void EditorScreen::checkInput()
 	}
 }
 
-
 bool EditorScreen::onExitClicked(const CEGUI::EventArgs& eargs)
 {
 	//std::cout << "Quitting the Game!\n";
 	m_currentState = ge::ScreenState::EXIT_APPLICATION;
+	return true;
+}
+
+bool EditorScreen::onRedPickerChanged() {
+	m_rColorVal = m_rSlider->getCurrentValue();
+	return true;
+}
+
+bool EditorScreen::onGreenPickerChanged() {
+	m_gColorVal = m_gSlider->getCurrentValue();
+	return true;
+}
+
+bool EditorScreen::onBluePickerChanged() {
+	m_bColorVal = m_bSlider->getCurrentValue();
 	return true;
 }
--- NinjaPlatformer/EditorScreen.h
+++ NinjaPlatformer/EditorScreen.h
@@ -2,6 +2,9 @@
 #include <GameEngineOpenGL\IGameScreen.h>
 #include <GameEngineOpenGL\Window.h>
 #include <GameEngineOpenGL\Camera2D.h>
+#include <GameEngineOpenGL\SpriteBatch.h>
+#include <GameEngineOpenGL\SpriteFont.h>
+#include <GameEngineOpenGL\GLSLProgram.h>
 #include <GameEngineOpenGL\GUI.h>
 #include "ScreenIndices.h"
 
@@ -31,20 +34,32 @@ public:
 	virtual void draw() override;
 
 #pragma endregion TODO: TEST GUI ELEMENTS
+
+	void initShaders();
 	
 private:
 	void initGUI();
 	void checkInput();
 	bool onExitClicked(const CEGUI::EventArgs& eargs);
+	bool onRedPickerChanged();
+	bool onGreenPickerChanged();
+	bool onBluePickerChanged();
 
 private:
-
 	ge::Window* m_window = nullptr;
 	ge::GUI m_gui;
 	ge::Camera2D m_camera; // Renders the scene
-	//int m_nextScreenIdx = SCREEN_INDEX_GAMEPLAY;
-	float m_colorPickerRed = 255.f;
-	float m_colorPickerGreen = 255.f;
-	float m_colorPickerBlue = 255.f;
+	ge::SpriteBatch m_spriteBatch;
+	ge::SpriteFont m_spriteFont;
+	ge::GLSLProgram m_textureProgram;// Shader for the textures
+	GLuint m_blankTexture = 0;
+
+	float m_rColorVal = 255.f;
+	float m_gColorVal = 0.f;
+	float m_bColorVal = 128.f;
+
+	CEGUI::Slider* m_rSlider = nullptr;
+	CEGUI::Slider* m_gSlider = nullptr;
+	CEGUI::Slider* m_bSlider = nullptr;
 };
 
